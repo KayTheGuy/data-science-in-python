08524a04a1d2353d408f2f47a71d2896ba350cd2
Kayhan Dehghani Mohammadi <kdehghan@sfu.ca>
2017-06-09 21:30:56 -0700

    Finish testing Central Limit Theorem and Mann–Whitney U-test

--- EX5/reddit_weekends.py
+++ EX5/reddit_weekends.py
@@ -41,8 +41,10 @@ def clean_data():
     global raw_counts
     global weekday_counts
     global weekend_counts
-    # extract year and add to a new column
+    # extract year, week number, and day number and add them as new columns
     raw_counts['year'] = pd.DatetimeIndex(raw_counts['date']).year
+    raw_counts['week_num'] = pd.DatetimeIndex(raw_counts['date']).week
+    # used for Fix 2 section in analyze_data()
     raw_counts['day_num'] = pd.DatetimeIndex(raw_counts['date']).weekday
 
     # filter data
@@ -60,43 +62,52 @@ def analyze_data():
         2) check normality for both original weekday and weekend comment counts
         3) check equality of covariances of original weekday and weekend comment counts
     """
+    week_comment_counts = weekday_counts['comment_count']
+    wknd_comment_counts = weekend_counts['comment_count']
 
-    week = weekday_counts['comment_count']
-    wknd = weekend_counts['comment_count']
-    
     # Fix 1: apply different (np.log, np.exp, np.sqrt, counts**2) transformation on data
     # np.exp results in overflow and counts**2 had poor results
     # between log and sqrt, sqrt had the better general result
 
-    week_sqrt = np.sqrt(week)
-    wknd_sqrt = np.sqrt(wknd)
+    week_comment_counts_sqrt = np.sqrt(week_comment_counts)
+    wknd_comment_counts_sqrt = np.sqrt(wknd_comment_counts)
+
+    # Fix 2: Central Limit Theorem
+    # combine all weekdays and weekend days from each week and take the mean
+    # of their (non-transformed) counts
+
+    weekly_weekday_comment_counts = weekday_counts.groupby(
+        ['year', 'week_num'], as_index=False).mean()['comment_count']
+    weekly_weekend_comment_counts = weekend_counts.groupby(
+        ['year', 'week_num'], as_index=False).mean()['comment_count']
 
     print(OUTPUT_TEMPLATE.format(
-        initial_ttest_p=stats.ttest_ind(week, wknd).pvalue,
-        initial_weekday_normality_p=stats.normaltest(week).pvalue,
-        initial_weekend_normality_p=stats.normaltest(wknd).pvalue,
-        initial_levene_p=stats.levene(week, wknd).pvalue,
-        transformed_weekday_normality_p=stats.normaltest(week_sqrt).pvalue,
-        transformed_weekend_normality_p=stats.normaltest(wknd_sqrt).pvalue,
-        transformed_levene_p=stats.levene(week_sqrt, wknd_sqrt).pvalue,
-        weekly_weekday_normality_p=0,
-        weekly_weekend_normality_p=0,
-        weekly_levene_p=0,
-        weekly_ttest_p=0,
-        utest_p=0,
+        initial_ttest_p=stats.ttest_ind(
+            week_comment_counts, wknd_comment_counts).pvalue,
+        initial_weekday_normality_p=stats.normaltest(
+            week_comment_counts).pvalue,
+        initial_weekend_normality_p=stats.normaltest(
+            wknd_comment_counts).pvalue,
+        initial_levene_p=stats.levene(
+            week_comment_counts, wknd_comment_counts).pvalue,
+        transformed_weekday_normality_p=stats.normaltest(
+            week_comment_counts_sqrt).pvalue,
+        transformed_weekend_normality_p=stats.normaltest(
+            wknd_comment_counts_sqrt).pvalue,
+        transformed_levene_p=stats.levene(
+            week_comment_counts_sqrt, wknd_comment_counts_sqrt).pvalue,
+        weekly_weekday_normality_p=stats.normaltest(
+            weekly_weekday_comment_counts).pvalue,
+        weekly_weekend_normality_p=stats.normaltest(
+            weekly_weekend_comment_counts).pvalue,
+        weekly_levene_p=stats.levene(
+            weekly_weekday_comment_counts, weekly_weekend_comment_counts).pvalue,
+        weekly_ttest_p=stats.ttest_ind(
+            weekly_weekday_comment_counts, weekly_weekend_comment_counts).pvalue,
+        utest_p=stats.mannwhitneyu(
+            week_comment_counts, wknd_comment_counts).pvalue,
     ))
 
-    # plt.hist(week)
-    # plt.title('weekdays')
-    # plt.show()
-    # plt.hist(wknd)
-    # plt.title('weekends')
-    # plt.show()
-
-    # answer to the last question: When are more Reddit comments posted in /r/canada, weekdays or weekends?
-    print (week.mean(axis=0))
-    print (wknd.mean(axis=0))
-    
 
 def main():
     """ Read the Reddit comments from gz file and answer the following question:
@@ -105,6 +116,7 @@ def main():
     read_data()
     clean_data()
     analyze_data()
+
 
 if __name__ == '__main__':
     main()
